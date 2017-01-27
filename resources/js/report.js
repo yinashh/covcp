0a16f7652a4c7ff87a992505aab8853ec41974fb
liucec <cecilia.m.liu@alumni.ubc.ca>
2017-01-27 12:32:15 -0800

    Google Maps API attempt

---
+++ resources/js/report.js
@@ -0,0 +1,27 @@
+/* 
+function initMap() {
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
+*/
+        
+$(document).ready(function(){
+    var uluru = {lat: -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 4,
+        center: uluru,
+        console.log('map');
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+    map();
+}
