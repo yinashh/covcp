08c29458368a2c7d1ec941bb34b1044776f6e2f4
yinashh <sunny_summer_sum@live.com>
2017-01-27 12:05:55 -0800

    Added php functionality
    Login checks for username and password in database.
    Report page displays filters consisting of single instance of database entries.

---
+++ search.php
@@ -0,0 +1,39 @@
+<?php
+include "db.php";
+include "functions.php";
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+    
+    <head>
+        <link rel="stylesheet" type="text/css" href="external/css/ionicons.min.css">
+        <link rel="stylesheet" type="text/css" href="resources/css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,300i,400" rel="stylesheet">
+        <title>Traffic Vision</title>
+    </head>
+    
+    <body>
+        <header>
+            <nav>
+                <div class="row">
+                    <a href="#"> <!--link to index.html-->
+                        <img src="resources/img/logo.png" alt="Traffic Vision Logo" class="nav-logo">
+                    </a>
+                    
+                    <ul class="main-nav">
+                        <li><a href="#">Home</a></li> <!--link to index.html-->
+                        <li><a href="#">Search</a></li> <!--link to search.html-->
+                        <li><a href="#">Report Generation</a></li> <!--link to report.html-->
+                        <li><a href="#">Help</a></li> <!--link to help.html-->
+                        <li><a href="#"><i class="ion-power"></i></a></li> <!--link to login.html-->
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        
+        <h1>Search</h1>
+        
+    </body>
+</html>
